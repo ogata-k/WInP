5a84ba0ff66cfd3434622da6b48ce72d3534ed74
ogata-k <ogtkzk712@gmail.com>
2024-07-06 18:01:56 +0900

    add navigation with the sample

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
--- app/src/main/java/com/ogata_k/mobile/winp/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/ogata_k/mobile/winp/presentation/activity/MainActivity.kt
@@ -3,14 +3,21 @@ package com.ogata_k.mobile.winp.presentation.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.ogata_k.mobile.winp.presentation.theme.WInPTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,15 +25,64 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             WInPTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navController = rememberNavController()
+
+                SetupRouting(navController = navController)
+            }
+        }
+    }
+}
+
+@Composable
+fun SetupRouting(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "screen1") {
+        composable(
+            route = "screen1"
+        ) { _ ->
+            Screen1(navController = navController)
+        }
+        composable(
+            route = "screen2/{name}",
+            arguments = listOf(navArgument("name") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val name: String = backStackEntry.arguments?.getString("name") ?: "android"
+            Screen2(navController = navController, name = name)
+        }
+    }
+}
+
+@Composable
+fun Screen1(navController: NavHostController) {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column {
+            Greeting(
+                name = "android",
+                modifier = Modifier.padding(innerPadding),
+            )
+            Button(onClick = {
+                navController.navigate("screen2/HOGE")
+            }) {
+                Text(text = "TAP HERE")
+            }
+        }
+    }
+}
+
+@Composable
+fun Screen2(navController: NavHostController, name: String) {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column {
+            Greeting(
+                name = name,
+                modifier = Modifier.padding(innerPadding),
+            )
+            Button(onClick = {
+                navController.navigate("screen1")
+            }) {
+                Text(text = "TAP HERE")
             }
         }
     }
@@ -34,10 +90,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
