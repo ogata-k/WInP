c9a4329dcfadc0dc6a44adb21c149e18fea956c6
ogata-k <ogtkzk712@gmail.com>
2024-07-06 18:54:11 +0900

    ベースとなるViewModelを作成

---
+++ app/src/main/java/com/ogata_k/mobile/winp/presentation/viewmodel/AbstractViewModel.kt
@@ -0,0 +1,38 @@
+package com.ogata_k.mobile.winp.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+/**
+ * UIの状態に変換する方法を提供するインターフェース
+ */
+interface ToUiState<out UiState> {
+    fun toUiState(): UiState
+}
+
+/**
+ * ViewModelStateの状態を変更することでUiStateを自動的に更新することができるViewModel
+ */
+abstract class AbstractViewModel<ViewModelState : ToUiState<UiState>, UiState>(
+    initialViewModelState: ViewModelState
+) : ViewModel() {
+    private val _viewModelState: MutableStateFlow<ViewModelState> =
+        MutableStateFlow(initialViewModelState)
+
+    protected fun readState(): ViewModelState {
+        return _viewModelState.value
+    }
+
+    protected fun updateState(newViewModelState: ViewModelState) {
+        _viewModelState.value = newViewModelState
+    }
+
+    val uiState: StateFlow<UiState> = _viewModelState
+        .map { v -> v.toUiState() }
+        .stateIn(viewModelScope, SharingStarted.Eagerly, _viewModelState.value.toUiState())
+}
