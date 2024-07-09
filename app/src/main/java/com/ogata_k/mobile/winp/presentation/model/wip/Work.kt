f79be2af7c8f5bc0b29b2511db33520eb4bb1750
ogata-k <ogtkzk712@gmail.com>
2024-07-09 21:19:07 +0900

    期限を指定しない場合の考慮が抜けていた

--- app/src/main/java/com/ogata_k/mobile/winp/presentation/model/wip/Work.kt
+++ app/src/main/java/com/ogata_k/mobile/winp/presentation/model/wip/Work.kt
@@ -10,11 +10,11 @@ data class Work(
     val title: String,
     val description: String,
     val progresses: List<ProgressDescription>,
-    val deadline: LocalDateTime,
+    val deadline: LocalDateTime?,
     val completedAt: LocalDateTime?,
     val cratedAt: LocalDateTime,
     val updatedAt: LocalDateTime,
 ) {
-    val isExpired: Boolean = deadline.isBefore(LocalDateTime.now())
+    val isExpired: Boolean = deadline?.isBefore(LocalDateTime.now()) ?: false
     val isCompleted: Boolean = completedAt != null
 }
