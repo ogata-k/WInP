a645ec47e6e8460705825825be0dd25d46b2e091
ogata-k <ogtkzk712@gmail.com>
2024-07-20 16:01:13 +0900

    ヘッダーを表示するScaffoldのヘルパーを実装

--- app/src/main/java/com/ogata_k/mobile/winp/presentation/page/work/index/WorkIndexScreen.kt
+++ app/src/main/java/com/ogata_k/mobile/winp/presentation/page/work/index/WorkIndexScreen.kt
@@ -2,9 +2,14 @@ package com.ogata_k.mobile.winp.presentation.page.work.index
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import androidx.paging.compose.LazyPagingItems
@@ -12,8 +17,10 @@ import androidx.paging.compose.collectAsLazyPagingItems
 import com.ogata_k.mobile.winp.R
 import com.ogata_k.mobile.winp.presentation.model.wip.Work
 import com.ogata_k.mobile.winp.presentation.widgert.common.PagingLoadColumn
+import com.ogata_k.mobile.winp.presentation.widgert.common.WithScaffoldSmallTopAppBar
 import com.ogata_k.mobile.winp.presentation.widgert.work.WorkItem
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WorkIndexScreen(navController: NavController, viewModel: WorkIndexVM) {
     val uiState: WorkIndexUiState by viewModel.uiState.collectAsStateWithLifecycle()
@@ -21,15 +28,27 @@ fun WorkIndexScreen(navController: NavController, viewModel: WorkIndexVM) {
     // collect fLow and remember state and launch event
     val workPagingItems: LazyPagingItems<Work> = uiState.workPagingData.collectAsLazyPagingItems()
 
+    // TODO PagingSourceをDBを使う形式に変更
+    // TODO 表示する日時を変更することでリストを更新する機能
 
-    PagingLoadColumn(
-        contentPadding = PaddingValues(
-            vertical = dimensionResource(id = R.dimen.padding_medium),
-            horizontal = dimensionResource(id = R.dimen.padding_medium_large),
-        ),
-        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium_large)),
-        pagingItems = workPagingItems,
-    ) { work ->
-        WorkItem(work) { /*TODO*/ }
+    WithScaffoldSmallTopAppBar(text = stringResource(id = R.string.app_name)) { modifier, appBar ->
+        Scaffold(
+            modifier = modifier,
+            topBar = { appBar() },
+        ) { padding ->
+            PagingLoadColumn(
+                modifier = Modifier.padding(padding),
+                contentPadding = PaddingValues(
+                    vertical = dimensionResource(id = R.dimen.padding_medium),
+                    horizontal = dimensionResource(id = R.dimen.padding_medium_large),
+                ),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium_large)),
+                pagingItems = workPagingItems,
+            ) { work ->
+                WorkItem(work) {
+                    // TODO
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/ogata_k/mobile/winp/presentation/widgert/common/WithScaffoldTopAppBar.kt
@@ -0,0 +1,156 @@
+package com.ogata_k.mobile.winp.presentation.widgert.common
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+
+// cf: https://developer.android.com/develop/ui/compose/components/app-bars?hl=ja
+
+/**
+ * 中央ぞろえの基本的なAppBarがついたScaffold用
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WithScaffoldCenteredSmallTopAppBar(
+    text: String,
+    modifier: Modifier = Modifier,
+    navigationIcon: @Composable () -> Unit = {},
+    actions: @Composable RowScope.() -> Unit = {},
+    windowInsets: WindowInsets = TopAppBarDefaults.windowInsets,
+    colors: TopAppBarColors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+        containerColor = MaterialTheme.colorScheme.primaryContainer,
+        titleContentColor = MaterialTheme.colorScheme.primary,
+    ),
+    scaffoldBuilder: @Composable (scaffoldModifier: Modifier, topAppBar: @Composable () -> Unit) -> Unit,
+) {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
+    scaffoldBuilder(Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)) {
+        CenterAlignedTopAppBar(
+            modifier = modifier,
+            colors = colors,
+            title = {
+                HeadlineMediumText(text = text)
+            },
+            navigationIcon = navigationIcon,
+            actions = actions,
+            windowInsets = windowInsets,
+            scrollBehavior = scrollBehavior,
+        )
+    }
+}
+
+/**
+ * 左寄せの基本的なAppBar
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WithScaffoldSmallTopAppBar(
+    text: String,
+    modifier: Modifier = Modifier,
+    navigationIcon: @Composable () -> Unit = {},
+    actions: @Composable RowScope.() -> Unit = {},
+    windowInsets: WindowInsets = TopAppBarDefaults.windowInsets,
+    colors: TopAppBarColors = TopAppBarDefaults.topAppBarColors(
+        containerColor = MaterialTheme.colorScheme.primaryContainer,
+        titleContentColor = MaterialTheme.colorScheme.primary,
+    ),
+    scaffoldBuilder: @Composable (scaffoldModifier: Modifier, topAppBar: @Composable () -> Unit) -> Unit,
+) {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
+    scaffoldBuilder(Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)) {
+        TopAppBar(
+            modifier = modifier,
+            colors = colors,
+            title = {
+                HeadlineMediumText(text = text)
+            },
+            navigationIcon = navigationIcon,
+            actions = actions,
+            windowInsets = windowInsets,
+            scrollBehavior = scrollBehavior,
+        )
+    }
+}
+
+/**
+ * 左寄せの中サイズの基本的なAppBar
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WithScaffoldMediumTopAppBar(
+    text: String,
+    modifier: Modifier = Modifier,
+    navigationIcon: @Composable () -> Unit = {},
+    actions: @Composable RowScope.() -> Unit = {},
+    windowInsets: WindowInsets = TopAppBarDefaults.windowInsets,
+    colors: TopAppBarColors = TopAppBarDefaults.mediumTopAppBarColors(
+        containerColor = MaterialTheme.colorScheme.primaryContainer,
+        titleContentColor = MaterialTheme.colorScheme.primary,
+    ),
+    scaffoldBuilder: @Composable (scaffoldModifier: Modifier, topAppBar: @Composable () -> Unit) -> Unit,
+) {
+    val scrollBehavior =
+        TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
+
+    scaffoldBuilder(Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)) {
+        MediumTopAppBar(
+            modifier = modifier,
+            colors = colors,
+            title = {
+                HeadlineMediumText(text = text)
+            },
+            navigationIcon = navigationIcon,
+            actions = actions,
+            windowInsets = windowInsets,
+            scrollBehavior = scrollBehavior,
+        )
+    }
+}
+
+/**
+ * 左寄せの大サイズの基本的なAppBar
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WithScaffoldLargeTopAppBar(
+    text: String,
+    modifier: Modifier = Modifier,
+    navigationIcon: @Composable () -> Unit = {},
+    actions: @Composable RowScope.() -> Unit = {},
+    windowInsets: WindowInsets = TopAppBarDefaults.windowInsets,
+    colors: TopAppBarColors = TopAppBarDefaults.largeTopAppBarColors(
+        containerColor = MaterialTheme.colorScheme.primaryContainer,
+        titleContentColor = MaterialTheme.colorScheme.primary,
+    ),
+    scaffoldBuilder: @Composable (scaffoldModifier: Modifier, topAppBar: @Composable () -> Unit) -> Unit,
+) {
+    val scrollBehavior =
+        TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
+
+    scaffoldBuilder(Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)) {
+        LargeTopAppBar(
+            modifier = modifier,
+            colors = colors,
+            title = {
+                HeadlineMediumText(text = text)
+            },
+            navigationIcon = navigationIcon,
+            actions = actions,
+            windowInsets = windowInsets,
+            scrollBehavior = scrollBehavior,
+        )
+    }
+}
