04ff0384897806ff0ac34e81ba69c71cf78f9dea
ogata-k <ogtkzk712@gmail.com>
2024-11-19 08:46:59 +0900

    mod: 期限が過ぎた後に完了させると期限が過ぎて未対応だった日にちを指定しても検索結果に現れない問題を修正

--- app/infra/src/main/java/com/ogata_k/mobile/winp/infra/database/dao/WorkWithWorkTodoDao.kt
+++ app/infra/src/main/java/com/ogata_k/mobile/winp/infra/database/dao/WorkWithWorkTodoDao.kt
@@ -17,37 +17,60 @@ interface WorkWithWorkTodoDao {
     /**
      * 指定した範囲で対応すべきタスクを検索する（リレーションで取得する進捗状況のソートはなし）
      * 外部で使う予定のメソッドではないので、_から始まる名前にしてある。
+     *
+     * 抽出条件
+     * ・期限が指定期間に含まれるか
+     * ・完了していてタスクの開始日時～タスクの完了日時が指定期間に含まれるか
+     * ・完了していなくて期限が過ぎているか
      */
     @Transaction
     @Query(
         """
-        SELECT works.* FROM works
-        WHERE (datetime(ended_at) <= datetime(:from) AND completed_at IS NULL)
-            OR (began_at IS NULL AND ended_at IS NULL)
+            SELECT works.* FROM works WHERE
+        """ +
+                //
+                // 期限が指定期間に含まれるか
+                //
+                """
+            (began_at IS NULL AND ended_at IS NULL)
             OR (datetime(began_at) <= datetime(:to) AND ended_at IS NULL)
             OR (began_at IS NULL AND datetime(ended_at) >= datetime(:from))
             OR (datetime(began_at) <= datetime(:to) AND datetime(ended_at) >= datetime(:from))
-        ORDER BY (ended_at IS NOT NULL AND datetime(ended_at) <= datetime(:from) AND completed_at IS NULL) DESC,
-        began_at IS NULL ASC, datetime(began_at) ASC, ended_at IS NULL ASC, datetime(ended_at) ASC,
-        work_id DESC
-        LIMIT :pageSize OFFSET :offset
+        """ +
+                //
+                // 完了していてタスクの開始日時～タスクの完了日時が指定期間に含まれるか
+                //
+                """
+            OR (completed_at IS NOT NULL AND (
+                (began_at IS NULL AND datetime(completed_at) >= datetime(:from))
+                OR (datetime(began_at) <= datetime(:to) AND datetime(completed_at) >= datetime(:from))
+            ))
+        """ +
+                //
+                // 完了していなくて期限が過ぎているか
+                //
+                """
+            OR (completed_at IS NULL AND datetime(ended_at) <= datetime(:from))
+        """ +
+                //
+                // その他条件
+                //
+                """
+            ORDER BY (ended_at IS NOT NULL AND datetime(ended_at) <= datetime(:from) AND completed_at IS NULL) DESC,
+            began_at IS NULL ASC, datetime(began_at) ASC, ended_at IS NULL ASC, datetime(ended_at) ASC,
+            work_id DESC
+            LIMIT :pageSize OFFSET :offset
         """
     )
     suspend fun _fetchWorksAtTheRangeWithoutSortRelation(
-        from: OffsetDateTime,
-        to: OffsetDateTime,
-        offset: Int,
-        pageSize: Int
+        from: OffsetDateTime, to: OffsetDateTime, offset: Int, pageSize: Int
     ): List<WorkWithWorkTodo>
 
     /**
      * 指定した範囲で対応すべきタスクを検索する
      */
     suspend fun fetchWorksAtTheRange(
-        from: OffsetDateTime,
-        to: OffsetDateTime,
-        offset: Int,
-        pageSize: Int
+        from: OffsetDateTime, to: OffsetDateTime, offset: Int, pageSize: Int
     ): List<WorkWithWorkTodo> {
         val works = _fetchWorksAtTheRangeWithoutSortRelation(from, to, offset, pageSize)
         return works.map { work -> work.copy(workTodos = work.workTodos.sortedBy { it.position }) }
