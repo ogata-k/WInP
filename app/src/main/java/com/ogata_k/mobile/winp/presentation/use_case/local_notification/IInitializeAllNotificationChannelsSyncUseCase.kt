0fded47c1ab1fd09396b41ce1c01776926c1c6ea
ogata-k <ogtkzk712@gmail.com>
2024-12-15 14:53:03 +0900

    fix: 通知チャネルを作成するときの通知権限があるかどうかのチェックと作成されてあった時のチェックが12L以前だとめんどくさそうな実装になっていた

--- app/src/main/java/com/ogata_k/mobile/winp/presentation/use_case/local_notification/IInitializeAllNotificationChannelsSyncUseCase.kt
+++ app/src/main/java/com/ogata_k/mobile/winp/presentation/use_case/local_notification/IInitializeAllNotificationChannelsSyncUseCase.kt
@@ -1,6 +1,5 @@
 package com.ogata_k.mobile.winp.presentation.use_case.local_notification
 
-import android.os.Build
 import com.ogata_k.mobile.winp.domain.component.LocalNotificationScheduler
 import com.ogata_k.mobile.winp.domain.use_case.local_notification.InitializeAllNotificationChannelsInput
 import com.ogata_k.mobile.winp.domain.use_case.local_notification.InitializeAllNotificationChannelsSyncUseCase
@@ -8,10 +7,6 @@ import com.ogata_k.mobile.winp.domain.use_case.local_notification.InitializeAllN
 class IInitializeAllNotificationChannelsSyncUseCase(private val localNotificationScheduler: LocalNotificationScheduler) :
     InitializeAllNotificationChannelsSyncUseCase {
     override fun call(input: InitializeAllNotificationChannelsInput) {
-        // バージョン１２L以前だとチャネルを作ったときに通知権限がリクエストされる
-        // そのため、１３以降のバージョンと通知権限依頼発行の挙動を合わせるために１３以降のみチャネルを作成する
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             localNotificationScheduler.initializeNotificationChannelsForAllLocalNotifyDiv()
-        }
     }
 }
