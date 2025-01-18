db427730d75f417ece96099024bdee24c1622909
ogata-k <ogtkzk712@gmail.com>
2025-01-18 15:53:40 +0900

    mod: ネイティブのデバッグシンボルも含めるように変更

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,9 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            ndk {
+                debugSymbolLevel = "FULL"
+            }
         }
     }
 
