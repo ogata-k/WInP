7202d86e197bb8187e7b9b754fed5e265b66e3b1
ogata-k <ogtkzk712@gmail.com>
2025-07-05 11:14:28 +0900

    Update build.gradle.kts
    リリースに向けて対応

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.ogata_k.mobile.winp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0"
+        versionCode = 5
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
