e72c3e21b211473078e9bebca3901d4df1c670fb
ogata-k <ogtkzk712@gmail.com>
2026-02-17 10:53:09 +0900

    リリース用にバージョンを更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.ogata_k.mobile.winp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.2"
+        versionCode = 7
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
